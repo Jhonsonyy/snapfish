55843b80d4c09fd28ef8d2ac13a71b2ae138b35c
Jhonsonyy <134149091+Jhonsonyy@users.noreply.github.com>
2024-04-12 15:07:51 -0700

    index.js

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express();
-const cors = require('cors');
 
-// Enable CORS for all origins
-app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
