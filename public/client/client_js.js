819659a6b51d13908726270a20a972175e1dc5b8
Jhonsonyy <134149091+Jhonsonyy@users.noreply.github.com>
2024-04-12 15:06:00 -0700

    Add files via upload

---
+++ public/client/client_js.js
@@ -0,0 +1,50 @@
+const Email = document.getElementById('Email');
+const Password = document.getElementById('Password')
+
+
+const button = document.getElementById('loginBtn');
+
+
+
+const getValues = async () => {
+    
+    const data = {
+        email: Email.value,
+        password: Password.value
+    };
+    
+    // Define the URL of the server
+    const url = 'http://192.168.1.198:3000/crad/Login'; // Replace with your server URL
+    
+    // Define the options for the fetch request
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json' // Specify the content type as JSON
+        },
+        body: JSON.stringify(data) // Convert data to JSON string
+    };
+    
+    // Send the POST request
+    fetch(url, options)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json(); // Parse the JSON response
+        })
+        .then(data => {
+            // Handle the response data
+            console.log('Response:', data);
+        })
+        .catch(error => {
+            // Handle any errors
+            console.error('Error:', error);
+        });
+
+
+
+}
+
+
+button.addEventListener('click', getValues)
