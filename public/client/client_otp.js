edc27805a5ea2d4c28e9a652625a8246886448c0
Jhonsonyy <134149091+Jhonsonyy@users.noreply.github.com>
2024-04-13 05:43:54 -0700

    Add files via upload

---
+++ public/client/client_otp.js
@@ -0,0 +1,31 @@
+const OTPInput = document.getElementById("otpForm");
+const submitBtn = document.getElementById("loginBtn");
+
+const getOtp = async () => {
+    const data = {
+        userOtp: OTPInput.value,
+        TypeOfAuth: "OTP"
+    };
+
+    const url = "http://192.168.1.198:3000/AuthOtp";
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            throw new Error('Network error or something else');
+        }
+        const responseData = await response.json();
+        console.log(responseData);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+submitBtn.addEventListener("click", getOtp);
